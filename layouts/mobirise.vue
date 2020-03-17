80b261910bdacdeaeb250889371f2383ef10dd42
guyspy <guyspy@gmail.com>
2020-03-18 01:20:53 +0800

    adjust color

--- layouts/mobirise.vue
+++ layouts/mobirise.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section class="menu cid-rTiAafnmIP" once="menu" id="menu1-c">
-      <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+      <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <div class="hamburger">
             <span></span>
@@ -18,7 +18,7 @@
               </a>
             </span> -->
             <span class="navbar-caption-wrap">
-              <nuxt-link class="navbar-caption text-white display-4" :to="localePath('index', $i18n.locale)">
+              <nuxt-link class="navbar-caption text-warning display-4" :to="localePath('index', $i18n.locale)">
                 La Pâtisserie Masako
               </nuxt-link>
             </span>
@@ -27,13 +27,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
             <li class="nav-item">
-              <a class="nav-link link text-white display-4" href="https://mobirise.co">
+              <a class="nav-link link text-warning display-4" href="https://mobirise.co">
                 <!-- <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span> -->
                 Services
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link link text-white display-4" href="https://mobirise.co">
+              <a class="nav-link link text-warning display-4" href="https://mobirise.co">
                 <!-- <span class="mbri-search mbr-iconfont mbr-iconfont-btn"></span> -->
                 About Us
               </a>
